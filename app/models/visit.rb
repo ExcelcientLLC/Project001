9624afdb10d646444d05296fd0be1e0ec19d9e14
Chris Loew <cloew@mymail.mines.edu>
2012-09-23 20:33:31 -0600

    Added Visit Scaffold

---
+++ app/models/visit.rb
@@ -0,0 +1,3 @@
+class Visit < ActiveRecord::Base
+  attr_accessible :visit_date
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923211706) do
+ActiveRecord::Schema.define(:version => 20120924010407) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20120923211706) do
     t.string   "state"
     t.string   "zip"
     t.text     "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "visits", :force => true do |t|
+    t.date     "visit_date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
