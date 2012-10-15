2fc768d4213bb40f44fa5c3fbbf7190772c9fead
Chris Loew <cloew@mymail.mines.edu>
2012-10-14 19:40:25 -0600

    Now can cycle through each individual visit

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,4 +1,9 @@
 module VisitsHelper
+    def previous_visit(visits, visit)
+        visit_id = visits.index(visit)
+        return visits[visit_id-1]
+    end
+    
     def next_visit(visits, visit)
         visit_id = visits.index(visit)
         return visits[visit_id+1]
