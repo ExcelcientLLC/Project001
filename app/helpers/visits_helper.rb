3aec74a8854847e73ec77cdd6fa805899a8ef89a
Chris Loew <cloew123@gmail.com>
2012-12-09 13:17:53 -0700

    Finished some of the goal stuff

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -20,4 +20,12 @@ module VisitsHelper
     def getGoalProgress(goal)
         return "$%d%s$%d dollars" % [goal.current_value, " out of ", goal.target_value]
     end
+
+    def getGoalProgressPercent(goal)
+        return (goal.current_value*100)/goal.target_value
+    end
+
+    def getGoalEndDate(goal)
+        return "Reaching goal of $%d in %d" % [goal.target_value, goal.target_date.year]
+    end
 end
