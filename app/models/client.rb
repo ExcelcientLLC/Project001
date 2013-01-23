8fbf9a71722b0d9a314001d288889bd7aafdc223
Alec Westerman <alecwesterman@gmail.com>
2013-01-22 20:17:29 -0700

    Added search field to client list

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,12 @@ class Client < ActiveRecord::Base
   attr_accessible :address, :city, :comment, :email, :first_name, :last_name, :phone, :state, :zip
   
   has_many :visits
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['first_name LIKE ? OR last_name LIKE ?', "%#{search}%", "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
