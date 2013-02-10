4f27bb283e0080fa706d462bf104114ea13ccce5
Alec Westerman <alecwesterman@gmail.com>
2013-02-09 17:54:14 -0700

    Beginnings or capybara tests for clients page

---
+++ spec/controllers/clients_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe ClientsController do
+
+  describe "GET #index" do
+    it "assigns all @clients" do
+      client = Client.create
+      get :index
+      assigns(:clients).should eq([client])
+    end
+
+#    it "assigns all @clients" do
+#      client1 = Client.create(first_name: "Hupert")
+#      client2 = Client.create(first_name: "Rupert")
+#      client3 = Client.create(first_name: "Oliver")
+
+#      assigns(:clients).should eq([client1, client3, client2])
+#    end
+		  
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+end
