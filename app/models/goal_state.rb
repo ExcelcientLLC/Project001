4454a7a79f224488cf8df6d4dd093f8de043fbb3
Chris Loew <cloew123@gmail.com>
2013-02-24 16:04:51 -0700

    Removed some accidentally committed debug text

--- app/models/goal_state.rb
+++ app/models/goal_state.rb
@@ -5,17 +5,12 @@ class GoalState < ActiveRecord::Base
   belongs_to :visit
 
   def setup(other_current_value, other_current_expenditures, other_visit, other_goal)
-    puts "Setting up the Goal State"
     updateState(other_current_value, other_current_expenditures)
     self.visit = other_visit
     self.goal = other_goal
-
-    puts @current_value
-    puts @current_expenditures
   end
 
   def updateState(other_current_value, other_current_expenditures)
-    puts "Updating the Goal State"
     self.current_value = other_current_value
     self.current_expenditures = other_current_expenditures
   end
