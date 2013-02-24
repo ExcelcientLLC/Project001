b987ec16ecdc422cf3c2da55f025917a662b5ee4
Chris Loew <cloew123@gmail.com>
2013-02-24 15:08:08 -0700

    Added some helpful functions to set the goal state

--- app/models/goal_state.rb
+++ app/models/goal_state.rb
@@ -3,4 +3,15 @@ class GoalState < ActiveRecord::Base
 
   belongs_to :goal
   belongs_to :visit
+
+  def setup(other_current_value, other_current_expenditures, other_visit, other_goal)
+    updateState(other_current_value, other_current_expenditures)
+    visit = other_visit
+    goal = other_goal
+  end
+
+  def updateState(other_current_value, other_current_expenditures)
+    current_value = other_current_value
+    current_expenditures = other_current_expenditures
+  end
 end
