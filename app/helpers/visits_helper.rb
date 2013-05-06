ef195a120b26e6a7a4ddd61f912ba05fb9363725
Chris Loew <cloew123@gmail.com>
2013-05-05 22:04:25 -0600

    Celaned up the ability to get the Background Color from a helper function

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -26,53 +26,61 @@ module VisitsHelper
     end
 
     def getEventLocationPercentage(events, event)
-        start_event = events.first
-        end_event = events.last
+      start_event = events.first
+      end_event = events.last
 
-        if event == start_event
-            return 0
-        end
+      if event == start_event
+          return 0
+      end
 
-        if event == end_event
-            return 100
-        end
+      if event == end_event
+          return 100
+      end
 
-        span = end_event.getDate() - start_event.getDate()
-        diff = event.getDate() - start_event.getDate()
+      span = end_event.getDate() - start_event.getDate()
+      diff = event.getDate() - start_event.getDate()
 
-        percentage = (diff.to_i*100)/span.to_i
-        return percentage*0.95
+      percentage = (diff.to_i*100)/span.to_i
+      return percentage*0.95
     end
 
     def getGoalProgress(goal)
-        return "$%d%s$%d dollars" % [goal.current_value, " out of ", goal.target_value]
+      return "$%d%s$%d dollars" % [goal.current_value, " out of ", goal.target_value]
     end
 
     def getGoalProgressPercent(goal)
-        if goal.current_value == nil
-            puts "Goal Current Value is nil"
-            return 0
-        else
-            return (goal.current_value*100)/goal.target_value
-        end
+      if goal.current_value == nil
+          puts "Goal Current Value is nil"
+          return 0
+      else
+          return (goal.current_value*100)/goal.target_value
+      end
     end
 
     def getGoalEndDate(goal)
-        return "Reaching goal of $%d in %d" % [goal.target_value, goal.target_date.year]
+      return "Reaching goal of $%d in %d" % [goal.target_value, goal.target_date.year]
     end
 
     def getVisitSubmitText(visit)
-        return visit.new_record? ? "Create New Visit" : "Update Visit Date"
+      return visit.new_record? ? "Create New Visit" : "Update Visit Date"
     end
     
     def getStylePosition(events, event)
-        if event == events.first
-            return "left:left;"
-        elsif event == events.last
-            return "float:right;"
-        else
-            return "left:%s%%;" % getEventLocationPercentage(events, event).to_s
-        end
+      if event == events.first
+          return "left:left;"
+      elsif event == events.last
+          return "float:right;"
+      else
+          return "left:%s%%;" % getEventLocationPercentage(events, event).to_s
+      end
+    end
+    
+    def getBackgroundColor(event, currentVisit)
+      if event == currentVisit
+        return "background-color:#fdb813;"
+      else
+        return ""
+      end
     end
     
 end
