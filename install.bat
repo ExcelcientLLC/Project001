53982215c35fe09873b4ece23c3e6d2cf0c4f9e7
Chris Loew <cloew123@gmail.com>
2013-05-28 20:14:12 -0600

    Renamed prosperion service script to client management service

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@ REM set CLIENT_MANAGEMENT_SERVER_DIR="C:\ClientManagementServer"
 set CLIENT_MANAGEMENT_SERVER_DIR="C:\dev\Excelcient\Prosperion"
 set PYTHON_DIR="C:\Python27"
 
-%PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\prosperion_service.py install
+%PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py install
 pause
