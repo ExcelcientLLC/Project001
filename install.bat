3ff7514cb75dad19d125255843152539499cabd7
Chris Loew <cloew123@gmail.com>
2013-05-29 01:01:49 -0600

    Install script can now copy Prosperion directory

--- install.bat
+++ install.bat
@@ -8,6 +8,8 @@ set PYTHON_DIR="C:\Python27"
 %~p0\python-2.7.msi
 %~p0\rubyinstaller-1.9.2-p290.exe
 
+xcopy %~p0\Prosperion %CLIENT_MANAGEMENT_SERVER_DIR% /E
+
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py --startup auto install
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py start
 pause
