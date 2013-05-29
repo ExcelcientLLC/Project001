e018baaebd6984c3b58abaa4509d24411fbd5bc6
Chris Loew <cloew123@gmail.com>
2013-05-29 10:18:29 -0600

    Updated install script to be back at the latest revision

--- install-dir/install.bat
+++ install-dir/install.bat
@@ -1,14 +1,16 @@
-
-echo %~p0
-
-REM set CLIENT_MANAGEMENT_SERVER_DIR="C:\ClientManagementServer"
-set CLIENT_MANAGEMENT_SERVER_DIR="C:\dev\Excelcient\Prosperion"
+set CLIENT_MANAGEMENT_SERVER_DIR="C:\ClientManagementServer"
 set PYTHON_DIR="C:\Python27"
+set RUBY_BIN_DIR="C:\Ruby193\bin"
 
 %~p0\python-2.7.msi
 %~p0\rubyinstaller-1.9.2-p290.exe
 
-xcopy %~p0\Prosperion %CLIENT_MANAGEMENT_SERVER_DIR% /E
+mkdir %CLIENT_MANAGEMENT_SERVER_DIR%
+xcopy /s %~p0\ClientManagementServer %CLIENT_MANAGEMENT_SERVER_DIR% 
+
+REM cd %CLIENT_MANAGEMENT_SERVER_DIR%
+REM START /B %RUBY_BIN_DIR%\bundle.bat install
+REM %RUBY_BIN_DIR%\rake.bat db:migrate
 
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py --startup auto install
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py start
