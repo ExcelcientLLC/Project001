2dd88e5b206884816fa6f049615e3f4904b79381
Chris Loew <cloew123@gmail.com>
2013-05-31 17:36:19 -0600

    Cleaned up the Paperclip Initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,17 +1,6 @@
 require "paperclip"
  
-Paperclip.options[:command_path] = 'C:\Program Files (x86)\ImageMagick-6.8.5-Q16'
-Paperclip.options[:swallow_stderr] = false
-
-puts Paperclip.options[:command_path]
-
 case Config::CONFIG['host_os']
     when /mswin|mingw/  # Windows
         Paperclip.options[:command_path] = 'C:\Program Files (x86)\ImageMagick-6.8.5-Q16'
 end
-
-class Paperclip::CommandLine
-  def full_path(binary)
-    [self.class.path, binary].compact.join((File::ALT_SEPARATOR||File::SEPARATOR))
-  end
-end if defined?(Paperclip::CommandLine)
