666b26f8fec56843a653ff7549a3b0428551ea9a
Chris Loew <cloew123@gmail.com>
2013-06-03 17:59:12 -0600

    Added a global helper method to create the new image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-def sortable(column, title = nil)
-  title ||= column.titleize
-  direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-  link_to title, params.merge(:sort => column, :direction => direction)
-end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, params.merge(:sort => column, :direction => direction)
+  end
+  
+  def newImageButton(destination, remote=true, method='get')
+    link_to image_tag("Nuvola_Red_Plus_small.png", size: "28x28"), destination, :remote => remote, :method => method
+  end
 end
