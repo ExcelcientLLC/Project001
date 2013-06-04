a483cae4917a760024d31817021a4ad2ce998c0f
Chris Loew <cloew123@gmail.com>
2013-06-03 18:07:17 -0600

    Added helper function to get the delete images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
     link_to title, params.merge(:sort => column, :direction => direction)
   end
   
+  def deleteImageButton(destination, message)
+    link_to image_tag("Nuvola_filesystems_trashcan_full.png", size: "40x40"), destination, method: :delete, remote: true, confirm: message
+  end
+  
   def newImageButton(destination, remote=true, method='get')
     link_to image_tag("Nuvola_Red_Plus_small.png", size: "28x28"), destination, :remote => remote, :method => method
   end
