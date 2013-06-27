ab86318fbf4570b6a626ace6a3cf998ff9bc26b4
Chris Loew <cloew123@gmail.com>
2013-06-27 15:55:45 -0600

    Uninstall script now removes the installed directry

--- install-dir/uninstall.bat
+++ install-dir/uninstall.bat
@@ -4,4 +4,7 @@ set CLIENT_MANAGEMENT_SERVER_DIR=%DRIVE%\ClientManagementServer
 set PYTHON_DIR=%DRIVE%\Python27\
 
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py remove
+
+cd  %DRIVE%
+CALL rmdir %CLIENT_MANAGEMENT_SERVER_DIR% /s
 pause
