63ebdd4db079d0affc7bb385edd88f2d6d53c52a
Chris Loew <cloew123@gmail.com>
2013-06-27 15:30:56 -0600

    Added an uninstall script that removes the service

--- install-dir/start.bat
+++ install-dir/start.bat
@@ -4,3 +4,4 @@ set CLIENT_MANAGEMENT_SERVER_DIR=%DRIVE%\ClientManagementServer
 set PYTHON_DIR=%DRIVE%\Python27\
 
 %PYTHON_DIR%\python.exe %CLIENT_MANAGEMENT_SERVER_DIR%\client_management_service.py restart
+pause
