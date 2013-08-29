9e7ccba4bab6bfaf6a6ab5f847ecb7e65dd190c6
Chris Loew <cloew123@gmail.com>
2013-08-29 15:57:59 -0600

    The graphs can no whandle if the goal is completed after the target date

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,10 @@
 module GoalsHelper
+  def getMaxDateForGoal(goal)
+    finalVisitDate = goal.goal_states.last.visit.visit_date
+    
+    if finalVisitDate > goal.target_date
+      return finalVisitDate
+    end
+    return goal.target_date
+  end
 end
