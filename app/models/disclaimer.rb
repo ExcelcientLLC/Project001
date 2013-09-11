29afde4d2470ab1f48b7490499657689654f33ac
Chris Loew <cloew123@gmail.com>
2013-09-11 12:01:44 -0600

    The first disclaimer is always shown on the Timeline page

--- app/models/disclaimer.rb
+++ app/models/disclaimer.rb
@@ -2,4 +2,13 @@ class Disclaimer < ActiveRecord::Base
   attr_accessible :body, :name
   validates_length_of :name, :maximum => 20
   has_one :visit
+  
+  def self.get_disclaimer
+    disclaimer = Disclaimer.all.first
+    if disclaimer.nil?
+      discalimer = Disclaimer.new
+      disclaimer.save
+    end
+    return disclaimer
+  end
 end
