d3be809ea9ccaff5bd23110a72593d1348c179f8
Chris Loew <cloew123@gmail.com>
2013-09-30 17:13:30 -0600

    UPdated the paperclip initializer to check the proper path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,5 +2,9 @@ require "paperclip"
  
 case RbConfig::CONFIG['host_os']
     when /mswin|mingw/  # Windows
+      if File.directory?('C:\Program Files (x86)')
         Paperclip.options[:command_path] = 'C:\Program Files (x86)\ImageMagick-6.8.5-Q16'
+      else
+        Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.8.5-Q16'
+      end
 end
